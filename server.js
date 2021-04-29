c91385fdffac74ebeec7c39d6604f767d0c161bb
ak8317 <ankitkumar8317@gmail.com>
2021-04-29 23:10:29 +0530

    change server files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 //connecting to database
 connectDB();
 
@@ -13,6 +14,15 @@ app.use(express.json({ extended: false }));
 //defining routes
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/techs', require('./routes/techs'));
+
+//serve static assests
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 //defining port
 const port = process.env.PORT || 5000;
